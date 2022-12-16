1d27acb8d3e86aaf61d87204417c4e4c4e502a50
Lawrence137 <lawrencekimani18@gmail.com>
2022-12-16 16:48:34 +0300

    error fix

--- index.js
+++ index.js
@@ -7,46 +7,36 @@ const description = document.getElementById('film-info'); // selects the element
 const ticketSpan = document.getElementById('ticket-num'); // selects the element with the id 'ticket-num'
 const buyTicketButton = document.getElementById('buy-ticket'); // selects the element with the id 'buy-ticket'
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 
 document.addEventListener("DOMContentLoaded", () => { // listens for the DOMContentLoaded event, which fires when the initial HTML document has been completely loaded and parsed
-  const fetchTitle = fetch("http://localhost:3000/films"); // sends a GET request to the specified endpoint
+  const fetchTitle = fetch("https://my-json-server.typicode.com/Lawrence137/phase-1-wk-3-code-challenge/db"); // sends a GET request to the specified endpoint
   fetchTitle.then(response => response.json()).then(data => list(data)); // parses the response as JSON and passes it to the 'list' function
 });
 
 function list(data) { // 'list' function takes in the data returned from the API request
 
-  for (let i = 0; i < data.length; i++) { // loop through each element in the data array
+  for (let i = 0; i < data.films.length; i++) { // loop through each element in the data array
     const mov = document.createElement('h5'); // create an h5 element
-    mov.textContent = data[i].title; // set the text content of the h5 element to the title of the current film in the loop
+    mov.textContent = data.films[i].title; // set the text content of the h5 element to the title of the current film in the loop
 
     films.appendChild(mov); // append the h5 element to the 'films' element
     mov.addEventListener('click', () => { // listen for a click event on the h5 element
-      poster.src = data[i].poster; // set the src attribute of the 'poster' element to the poster image of the current film in the loop
-      title.textContent = data[i].title; // set the text content of the 'title' element to the title of the current film in the loop
-      runtime.textContent = data[i].runtime; // set the text content of the 'runtime' element to the runtime of the current film in the loop
-      showtime.textContent = data[i].showtime; // set the text content of the 'showtime' element to the showtime of the current film in the loop
-      description.textContent = data[i].description; // set the text content of the 'description' element to the description of the current film in the loop
-      ticketSpan.textContent = (data[i].capacity - data[i].tickets_sold); // set the text content of the 'ticketSpan' element to the number of available tickets for the current film in the loop
+      poster.src = data.films[i].poster; // set the src attribute of the 'poster' element to the poster image of the current film in the loop
+      title.textContent = data.films[i].title; // set the text content of the 'title' element to the title of the current film in the loop
+      runtime.textContent = data.films[i].runtime; // set the text content of the 'runtime' element to the runtime of the current film in the loop
+      showtime.textContent = data.films[i].showtime; // set the text content of the 'showtime' element to the showtime of the current film in the loop
+      description.textContent = data.films[i].description; // set the text content of the 'description' element to the description of the current film in the loop
+      ticketSpan.textContent = (data.films[i].capacity - data.films[i].tickets_sold); // set the text content of the 'ticketSpan' element to the number of available tickets for the current film in the loop
       updateBuyTicketButton(); // call the 'updateBuyTicketButton' function
     });
     // show the first film when the page loads
     // set the values of the elements to the corresponding values of the first film in the 'data' array
-    poster.src = data[0].poster;
-    title.textContent = data[0].title;
-    runtime.textContent = data[0].runtime;
-    showtime.textContent = data[0].showtime;
-    description.textContent = data[0].description;
-    ticketSpan.textContent = (data[0].capacity - data[0].tickets_sold);
+    poster.src = data.films[0].poster;
+    title.textContent = data.films[0].title;
+    runtime.textContent = data.films[0].runtime;
+    showtime.textContent = data.films[0].showtime;
+    description.textContent = data.films[0].description;
+    ticketSpan.textContent = (data.films[0].capacity - data.films[0].tickets_sold);
     updateBuyTicketButton(); // update the state of the 'buyTicketButton'
   }
 }
